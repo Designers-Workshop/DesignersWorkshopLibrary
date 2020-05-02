984600d05775b1b45b0dff61438f773a2183f7c5
Jeff L <51171427+LebJe@users.noreply.github.com>
2020-05-02 15:25:37 -0400

    Make everything in Types.swift public.

--- Sources/DesignersWorkshopLibrary/Types.swift
+++ Sources/DesignersWorkshopLibrary/Types.swift
@@ -4,58 +4,58 @@ import PostgresClientKit
 // MARK: - Global Structs.
 /// This struct represents the user that is current logged in.
 public struct User {
-	let id: Int
-	let name: String
-	let email: String
-	let address: String
-	let username: String
-	var password: String
-	let dateTimeCreated: PostgresTimestampWithTimeZone
-	let zone: String
-	let isAdmin: Bool
-	let profilePic: PostgresByteA
-	var forgotPasswordID: String?
+	public let id: Int
+	public let name: String
+	public let email: String
+	public let address: String
+	public let username: String
+	public var password: String
+	public let dateTimeCreated: PostgresTimestampWithTimeZone
+	public let zone: String
+	public let isAdmin: Bool
+	public let profilePic: PostgresByteA
+	public var forgotPasswordID: String?
 }
 
 /// This struct represents a single order by the user.
 public struct Order {
-	let id: Int
-	let user: User
-	var productList: [Product]
-	let orderDateTime: PostgresTimestampWithTimeZone
-	let zone: String
+	public let id: Int
+	public let user: User
+	public var productList: [Product]
+	public let orderDateTime: PostgresTimestampWithTimeZone
+	public let zone: String
 }
 
 /// This struct represents a product in an order.
 public struct Product: Codable {
-	let id: Int
-	let name: String
-	let price: Double
-	let image: Data?
+	public let id: Int
+	public let name: String
+	public let price: Double
+	public let image: Data?
 }
 
 /// This struct represents a page on the "Activities" , "Projects",  and "About Us" tabs.
 public struct Page: Codable, Hashable {
-	let id: Int
-	let title: String
-	let linkTitle: String
-	let contents: String?
-	let image: Data?
-	let dropdown: Dropdown
-	let type: String
+	public let id: Int
+	public let title: String
+	public let linkTitle: String
+	public let contents: String?
+	public let image: Data?
+	public let dropdown: Dropdown
+	public let type: String
 }
 
 public struct Dropdown: Codable, Hashable {
-	let id: Int
-	let name: String
+	public let id: Int
+	public let name: String
 }
 
 /// This struct represents a sketch uploaded by a user.
 public struct Sketch {
-	let id: Int
-	let user: User
-	let name: String
-	let image: Data
-	let dateTimeSubmitted: PostgresTimestampWithTimeZone
-	let zone: String
+	public let id: Int
+	public let user: User
+	public let name: String
+	public let image: Data
+	public let dateTimeSubmitted: PostgresTimestampWithTimeZone
+	public let zone: String
 }
